2ce4efdb106a74545d5da512ee6e88660e25fcd4
jhadvig <jhadvig@redhat.com>
2013-10-01 23:24:09 +0200

    redo

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,48 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+
+
+$(window).ready(function(){
+	FONDAK_Init();
+	console.log("pipise");
+});
+
+function FONDAK_Init(){
+
+	var endDate = "March 28, 2014 12:00:00";
+	console.log("pipise");
+
+	// $('.carousel').carousel({
+	// 	interval: 3000
+	// });
+
+	$("#owl-demo").owlCarousel({
+    	// navigation : true
+    	items: 3,
+    	autoPlay : 3000,
+    	itemsMobile : false,
+    	rewindSpeed: 1500,
+    	stopOnHover: true,
+  	});
+
+  	$('.countdown.styled').countdown({
+        date: endDate,
+        render: function(data) {
+        	$(this.el).html("<div><span>dni</span>" + this.leadingZeros(data.days, 3) + "</div><div><span>hod</span>" + this.leadingZeros(data.hours, 2) + "</div><div><span>min</span>" + this.leadingZeros(data.min, 2) + "</div><div><span>sek</span>" + this.leadingZeros(data.sec, 2) + "</div>");
+        }
+   	});
+
+}
