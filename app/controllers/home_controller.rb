b7f38d5625fa02612e8fc5df1945f1b8ac81ddbf
jhadvig <jhadvig@redhat.com>
2013-12-23 14:06:34 +0100

    contacts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,27 @@
 class HomeController < ApplicationController
 
   def index
-    carousel_images = *(1..29)
+    carousel_images = *(1..30)
     @picked_images = carousel_images.sample(10)
+  end
+
+  def discusion
+
+  end
+
+  def photo
+
+  end
+
+  def history
+
+  end
+
+  def sponsores
+
+  end
+
+  def contact
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Fondak::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match 'photo' => 'home#photo'
+  match 'discusion' => 'home#discusion'
+  match 'history' => 'home#history'
+  match 'sponsores' => 'home#sponsores'
+  match 'contact' => 'home#contact'
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
